91c69c677a3a0c3630664e01258a4150e4a5f9f4
O-Seok <totkfa789@gmail.com>
2020-05-27 12:39:21 +0900

    test_code

--- test_code.py
+++ test_code.py
@@ -42,26 +42,26 @@ class Stack:
         assert self.stack == [], '비어있지 않다!!!'
 
 
-n = int(input())
-stack = Stack()
-
-for i in range(n):
-    word = input().split()
-
-    command = word[0]
-
-    if command == 'push':
-        stack.do_push(word[1])
-    elif command == 'top':
-        print(stack.do_top())
-    elif command == 'size':
-        print(stack.do_size())
-    elif command == 'empty':
-        print(stack.do_empty())
-    elif command == 'pop':
-        print(stack.do_pop())
-    else:
-        print('no command !')
-
-
+# n = int(input())
+# stack = Stack()
+#
+# for i in range(n):
+#     word = input().split()
+#
+#     command = word[0]
+#
+#     if command == 'push':
+#         stack.do_push(word[1])
+#     elif command == 'top':
+#         print(stack.do_top())
+#     elif command == 'size':
+#         print(stack.do_size())
+#     elif command == 'empty':
+#         print(stack.do_empty())
+#     elif command == 'pop':
+#         print(stack.do_pop())
+#     else:
+#         print('no command !')
+#
+#
 
