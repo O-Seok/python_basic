54459c6f7912a5807efd73ba19e89d1d09d90714
O-Seok <totkfa789@gmail.com>
2020-05-27 12:38:44 +0900

    feat: Create unittest Create unittest.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
+# 단위 테스트를 해본다.
+# test_code.py 안의 Stack 클래스를 가져온다.
+# Stack 클래스를 테스트 해본다!
+
 import unittest
 from test_code import Stack
-
 
 class CustomTests(unittest.TestCase):
     def setUp(self):
@@ -12,3 +15,6 @@ class CustomTests(unittest.TestCase):
 
     def test_empty(self):
         assert self.s.do_empty() == 1
+
+if __name__ == '__main__':
+    unittest.main()
